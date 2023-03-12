785602d9fe0bcc0ab47671631b083e2f29bee0bb
c0d3rn0t <c0d3rn0t@gmail.com>
2023-03-12 18:27:17 +0800

    moved private info to dev.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+
+const { User } = require("./model/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://codernot13:YNWzf622K09015Ii@react-blog.uvirfv8.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     console.log("DB connected");
   })
@@ -14,8 +17,20 @@ mongoose
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => res.status(200).json(`User ${req.body.name} added!`))
+    .catch((err) => res.status(400).json(`Unable to add user. Error: ${err}`));
 });
 
 app.listen(5000, () => {
